a23782ff655bf2954bf6f25ca82c467aa72a659f
John Henly A. Montera <monterahens@gmail.com>
2022-08-24 14:01:10 +0800

    Initial Commit

--- app/src/main/java/com/henzmontera/cap102_plantapp/ThirdFragment.java
+++ app/src/main/java/com/henzmontera/cap102_plantapp/ThirdFragment.java
@@ -3,10 +3,16 @@ package com.henzmontera.cap102_plantapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,11 +59,34 @@ public class ThirdFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootview = inflater.inflate(R.layout.fragment_third, container, false);
+        
+        Button mangoapplebutton = rootview.findViewById(R.id.applemangobutton);
+        Button mangocarabaobutton = rootview.findViewById(R.id.carabaomangobutton);
+        Button mangoindianbutton = rootview.findViewById(R.id.indianmangobutton);
+        
+        mangoapplebutton.setOnClickListener(view -> {
+           Toast.makeText(getActivity(), "Apple Mango", Toast.LENGTH_LONG).show();
+           Log.d("Apple Mango","Apple Mango");
+        });
+        
+        mangocarabaobutton.setOnClickListener(view -> {
+            Toast.makeText(getActivity(), "Carabao Mango", Toast.LENGTH_LONG).show();
+            Log.d("Carabao Mango","Carabao Mango");
+        });
+        
+        mangoindianbutton.setOnClickListener(view -> {
+            Toast.makeText(getActivity(), "Indian Mango", Toast.LENGTH_LONG).show();
+            Log.d("Indian Mango","Indian Mango");
+        });
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false);
     }
