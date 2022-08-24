b2d421da2f8add7a01d6ca1691a54d41bdfccead
John Henly A. Montera <monterahens@gmail.com>
2022-08-24 16:41:18 +0800

    Update on the Apple Mango Img Proc and Nav on the 3rd fragment

---
+++ app/src/main/java/com/henzmontera/cap102_plantapp/AppleMangoActivity.java
@@ -0,0 +1,129 @@
+package com.henzmontera.cap102_plantapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.henzmontera.cap102_plantapp.ml.MangaApple;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class AppleMangoActivity extends AppCompatActivity {
+
+    TextView result, confidence;
+    ImageView imageView;
+    Button picture;
+    int imageSize = 224;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apple_mango);
+
+        result = findViewById(R.id.resultAM);
+        confidence = findViewById(R.id.confidenceAM);
+        imageView = findViewById(R.id.imageViewAM);
+        picture = findViewById(R.id.buttonAM);
+
+        picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Launch camera if we have permission
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1);
+                } else {
+                    //Request camera permission if we don't have it.
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+    }
+
+    public void classifyImage(Bitmap image){
+        try {
+
+            MangaApple model = MangaApple.newInstance(getApplicationContext());
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+            // get 1D array of 224 * 224 pixels in image
+            int [] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
+            int pixel = 0;
+            for(int i = 0; i < imageSize; i++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+                }
+            }
+            inputFeature0.loadBuffer(byteBuffer);
+            // Runs model inference and gets result.
+            MangaApple.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            // find the index of the class with the biggest confidence.
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for(int i = 0; i < confidences.length; i++){
+                if(confidences[i] > maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+
+            String[] classes =
+                    {"Large_MA_OR", "Large_MA_R", "Large_MA_RD", "Large_MA_ROT", "Large_MA_UR",
+                            "Medium_MA_OR", "Medium_MA_R", "Medium_MA_RD", "Medium_MA_ROT", "Medium_MA_UR",
+                            "SMALL_MA_OR", "SMALL_MA_R", "SMALL_MA_RD", "SMALL_MA_ROT", "SMALL_MA_UR"};
+
+            result.setText(classes[maxPos]);
+
+            String s = "";
+            for(int i = 0; i <= maxPos; i++){
+                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100);
+            }
+
+            confidence.setText(confidences[maxPos] * 100 + " %");
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            int dimension = Math.min(image.getWidth(), image.getHeight());
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+            imageView.setImageBitmap(image);
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+            classifyImage(image);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
--- app/src/main/java/com/henzmontera/cap102_plantapp/ThirdFragment.java
+++ app/src/main/java/com/henzmontera/cap102_plantapp/ThirdFragment.java
@@ -1,11 +1,9 @@
 package com.henzmontera.cap102_plantapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,73 +12,30 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ThirdFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ThirdFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ThirdFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ThirdFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ThirdFragment newInstance(String param1, String param2) {
-        ThirdFragment fragment = new ThirdFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootview = inflater.inflate(R.layout.fragment_third, container, false);
-        
+
         Button mangoapplebutton = rootview.findViewById(R.id.applemangobutton);
         Button mangocarabaobutton = rootview.findViewById(R.id.carabaomangobutton);
         Button mangoindianbutton = rootview.findViewById(R.id.indianmangobutton);
-        
+
         mangoapplebutton.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), AppleMangoActivity.class);
+            startActivity(intent);
            Toast.makeText(getActivity(), "Apple Mango", Toast.LENGTH_LONG).show();
            Log.d("Apple Mango","Apple Mango");
         });
-        
+
         mangocarabaobutton.setOnClickListener(view -> {
             Toast.makeText(getActivity(), "Carabao Mango", Toast.LENGTH_LONG).show();
             Log.d("Carabao Mango","Carabao Mango");
         });
-        
+
         mangoindianbutton.setOnClickListener(view -> {
             Toast.makeText(getActivity(), "Indian Mango", Toast.LENGTH_LONG).show();
             Log.d("Indian Mango","Indian Mango");
@@ -88,6 +43,6 @@ public class ThirdFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        return rootview;
     }
 }
