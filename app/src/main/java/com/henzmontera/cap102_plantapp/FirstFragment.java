e0e7788f0116e5663224d5ca1109380d2383749d
BnkuBnku <65868441+BnkuBnku@users.noreply.github.com>
2022-09-01 04:56:55 +0800

    User Profile 0.1

--- app/src/main/java/com/henzmontera/cap102_plantapp/FirstFragment.java
+++ app/src/main/java/com/henzmontera/cap102_plantapp/FirstFragment.java
@@ -21,51 +21,6 @@ import androidx.fragment.app.Fragment;
  */
 public class FirstFragment extends Fragment {
 
-    // Kani Delete
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FirstFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    // Hantod Dria
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- app/src/main/java/com/henzmontera/cap102_plantapp/MainActivity.java
+++ app/src/main/java/com/henzmontera/cap102_plantapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.henzmontera.cap102_plantapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -63,33 +65,34 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.Login:
+            case R.id.Profile:
                 drawer.closeDrawers();
-                Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.Register:
-                drawer.closeDrawers();
-                Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
                 break;
             case R.id.Timer:
                 drawer.closeDrawers();
-                Toast.makeText(this, "Timer", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Timer", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.Guide:
                 drawer.closeDrawers();
-                Toast.makeText(this, "Guide", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.DrawerOption:
-                drawer.closeDrawers();
-                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Guide", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.DrawerAuthor:
                 drawer.closeDrawers();
-                Toast.makeText(this, "Authors", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Authors", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.DrawerSocialMedia:
                 drawer.closeDrawers();
-                Toast.makeText(this, "Social Media", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Social Media", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.DrawerOption:
+                drawer.closeDrawers();
+                Toast.makeText(MainActivity.this, "Option", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.DrawerLogout:
+                drawer.closeDrawers();
+                Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
                 break;
         }
         return true;
