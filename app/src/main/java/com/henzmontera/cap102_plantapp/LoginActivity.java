e83dd181c4df7e3f0c5801a2d81a3ff98611be9c
John Henly A. Montera <monterahens@gmail.com>
2022-09-02 10:13:15 +0800

    Initial Commit

--- app/src/main/java/com/henzmontera/cap102_plantapp/LoginActivity.java
+++ app/src/main/java/com/henzmontera/cap102_plantapp/LoginActivity.java
@@ -79,20 +79,65 @@ public class LoginActivity extends AppCompatActivity {
 
         LoginButton.setOnClickListener(view -> {
 
-            myDB = openOrCreateDatabase("IntroSlideCheckStatus.db", 0, null);
-            Cursor ma_checkbox = myDB.rawQuery("SELECT COUNT(*) as count FROM logintomaincheckbox WHERE logintomaincheckbox.status = ?;", new String[] {"enable"});
-            while(ma_checkbox.moveToNext()){
-                a_test = ma_checkbox.getColumnIndex("count");
-                sa_test = ma_checkbox.getString(a_test);
-            }
-            if(sa_test.equals("1")){
-                Intent intent = new Intent(LoginActivity.this, LogToMainSlides.class);
-                startActivity(intent);
-            } else {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-            myDB.close();
+            /*
+            String url = "http://172.21.48.1/networkingbased/LoginUser.php";
+            RequestQueue request = Volley.newRequestQueue(view.getContext());
+            JsonArrayRequest RRequest = new JsonArrayRequest(
+                    Request.Method.POST,
+                    url,
+                    null,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            try {
+                                for (int a = 0; a < response.length(); a++) {
+                                    JSONObject object = response.getJSONObject(a);
+                                    userArray[a] = object.getString("username");
+                                    passArray[a] = object.getString("userpassword");
+                                }
+                            } catch (JSONException e) {
+                                Toast.makeText(view.getContext(), "Failed to login." + e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                            for (int a = 0; a < userArray.length ; a++){
+                                if(UserEditText.getText().toString().equals(userArray[a]) && PasswordEditText.getText().toString().equals(passArray[a])){ */
+                                        myDB = openOrCreateDatabase("IntroSlideCheckStatus.db", 0, null);
+                                        Cursor ma_checkbox = myDB.rawQuery("SELECT COUNT(*) as count FROM logintomaincheckbox WHERE logintomaincheckbox.status = ?;", new String[] {"enable"});
+                                        while(ma_checkbox.moveToNext()){
+                                            a_test = ma_checkbox.getColumnIndex("count");
+                                            sa_test = ma_checkbox.getString(a_test);
+                                        }
+                                        if(sa_test.equals("1")){
+                                            Intent intent = new Intent(LoginActivity.this, LogToMainSlides.class);
+                                            startActivity(intent);
+                                        } else {
+                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                            startActivity(intent);
+                                        }
+                                        myDB.close();
+                               /* }
+                                else{
+                                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(view.getContext(), "Failed to login.\n In ErrorListener \n" + error.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+            ) {
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> param = new HashMap<>();
+                    param.put("username", user);
+                    param.put("userpassword", pass);
+                    return param;
+                }
+            };
+            request.add(RRequest); */
         });
     }
 
