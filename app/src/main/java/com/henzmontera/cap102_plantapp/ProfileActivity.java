c961c646c54f527bb52b25eef2d6f9d9563a61a4
BnkuBnku <65868441+BnkuBnku@users.noreply.github.com>
2022-09-02 10:50:36 +0800

    User Profile 0.2

---
+++ app/src/main/java/com/henzmontera/cap102_plantapp/ProfileActivity.java
@@ -0,0 +1,75 @@
+package com.henzmontera.cap102_plantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    Button EditProfile; //I doubt to use this
+    TextView UsernameProfile;
+    TextView emptyView;
+    ImageView UserBackgroundProfile;
+    ImageView UserPictureProfile;
+
+    FloatingActionButton BackButtonProfile;
+    FloatingActionButton AddButtonThread;
+
+    RecyclerView recyclerview;
+
+    // to check whether sub FAB buttons are visible or not.
+    Boolean isAllFabsVisible;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        //FAB Button
+        BackButtonProfile = findViewById(R.id.BackButtonProfile);
+        AddButtonThread = findViewById(R.id.AddThreadButton);
+
+        emptyView = findViewById(R.id.TextviewEmpty);
+        UsernameProfile = findViewById(R.id.TextviewUsernameProfile);
+
+        UserPictureProfile = findViewById(R.id.ImageProfileView);
+        UserBackgroundProfile = findViewById(R.id.ImageProfileBackground);
+
+        recyclerview = findViewById(R.id.recyclerView);
+
+
+
+        //If dataset empty, Empty Textview visible, otherwise dataset visible
+//        if (dataset.isEmpty()) {
+//            recyclerview.setVisibility(View.GONE);
+//            emptyView.setVisibility(View.VISIBLE);
+//        }
+//        else {
+//            recyclerview.setVisibility(View.VISIBLE);
+//            emptyView.setVisibility(View.GONE);
+//        }
+
+        // make the boolean variable as false, as all the
+        // action name texts and all the sub FABs are invisible
+        isAllFabsVisible = true;
+
+        BackButtonProfile.setOnClickListener(view -> {
+            onBackPressed();
+        });
+
+        AddButtonThread.setOnClickListener(view -> {
+            Intent intent = new Intent(ProfileActivity.this, AddThreadActivity.class);
+            startActivity(intent);
+        });
+
+
+
+    }
+}
